0ba38fbfe1e526e47ce21c0ce1bd4769e3ba196f
huyhoangm2 <hoangken760@gmail.com>
2023-02-07 20:59:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import styles from "./App.module.css";
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Login</h1>
+      <form>
+        <p className={styles.fieldTitle}>nhập email:</p>
+        <input
+          name="email"
+          type="text"
+          className={styles.input}
+        />
+        <p className={styles.fieldTitle}>nhập password:</p>
+        <input
+          name="password"
+          type="password"
+          className={styles.input}
+        />
+        <br />
+        <label>
+          <input
+          className={styles.checkbox}
+            name="isRead"
+            type="checkbox" />I read and accept the privacy policy:
+        </label>
+
+        <p><button className={styles.btnSubmit}>submit nè</button></p>
+        
+      </form>
     </div>
   );
 }
-
-export default App;
